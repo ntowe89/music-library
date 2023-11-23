fc99040dc4607bf54e310bf331c0a629ac020aa5
Nathan Towe <nathanwtowe89@gmail.com>
2023-11-22 19:41:38 -0500

    fetch music data with useEffect

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import Gallery from './Components/Gallery/Gallery';
 import SearchBar from './Components/SearchBar';
 
@@ -9,7 +9,25 @@ function App() {
   const [message, setMessage] = useState('Search for Music')
   const[data, setData] = useState([])
   
-  
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = 'https://itunes.apple.com/search?term=black%20sabbath'
+      const response = await fetch(url)
+      const data = await response.json()
+
+      //handling no data found
+      if (data.results) {
+        setData(data.results)
+      }
+      else{
+        setData([])
+        setMessage('Not Found')
+      }
+    }
+
+    fetchData()
+  }, [search])
+
   return (
     <div>
       <SearchBar />
